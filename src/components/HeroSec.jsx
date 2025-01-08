ce86b4e7a2e7599dbd2c271fa8b3896e7902102c
bhagirathpaliyal <bhagirathpaliyal@gmail.com>
2025-01-08 12:57:28 +0530

    update:Herosec

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 const HeroSec = () => {
   return (
-    <div id='Home' className='bg-gradient-to-r from-[#091535] to-[#0d3065] w-full h-[90dvh]'>
-      <ul className='pt-[40px] text-secondary flex flex-col justify-center items-center font-bold text-[25px] gap-[20px]'>
+    <div id='Home' className='bg-gradient-to-r from-[#091535] to-[#0d3065] w-full h-[75dvh]'>
+      <ul className='pt-[40px] text-secondary flex flex-col justify-center items-center font-bold max-md:text-[20px] text-[26px] gap-[20px]'>
         <li className='animated-item animation-delay-100'>A</li>
         <li className='animated-item animation-delay-200'>Legacy</li>
         <li className='animated-item animation-delay-300'>of</li>
-        <li className='animated-item animation-delay-400'>Proven Excellence</li>
-        <li className='animated-item animation-delay-500'>in</li>
-        <li className='animated-item animation-delay-600'>Litigation</li>
+        <li className='animated-item animation-delay-400 text-center'>Proven Excellence in Litigation</li>
         <li className='animated-item animation-delay-700'>for</li>
         <li className='animated-item animation-delay-800'>Over Three Decades</li>
       </ul>
